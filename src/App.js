58ccb61ba207f49ec1460c2652299c114119d333
Mubeen Tariq <166198879+Mubeen35951@users.noreply.github.com>
2024-11-28 22:48:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
